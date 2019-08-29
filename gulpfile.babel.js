824e398036522c7fe84b59740d887632f53d2b5a
Pawel Christow <pawel.christow@gmail.com>
2019-08-29 20:32:44 +1000

    Add gulp tasks which are going to build the app and will serve it

---
+++ gulpfile.babel.js
@@ -0,0 +1,49 @@
+import gulp from 'gulp';
+import sass from 'gulp-sass';
+import browserSync from 'browser-sync';
+
+const server = browserSync.create();
+
+function scripts() {
+  return gulp.src(['src/assets/js/*.js']).pipe(gulp.dest('dist/assets/js'));
+}
+
+function styles() {
+  return gulp
+    .src(['src/assets/scss/styles.scss'])
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/assets/css'));
+}
+
+function template() {
+  return gulp.src(['src/index.html']).pipe(gulp.dest('dist/'));
+}
+
+function reload(done) {
+  server.reload();
+  done();
+}
+
+function serve(done) {
+  server.init({
+    server: './dist'
+  });
+  done();
+}
+
+function build(done) {
+  template();
+  scripts();
+  styles();
+  done();
+}
+
+function watch() {
+  gulp.watch('./src/assets/scss/**/*.scss', gulp.series(styles, reload));
+  gulp.watch('./src/assets/js/**/*.js', gulp.series(scripts, reload));
+  gulp.watch('./src/*.html', gulp.series(template, reload));
+}
+
+const demo = gulp.series(build, serve, watch);
+
+export default demo;

502575bb733de50e32c56755006cc8027257361c
Pawel Christow <pawel.christow@gmail.com>
2019-08-29 22:53:07 +1000

    add flower and drone layers functionality

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,14 +1,79 @@
 (() => {
   const mapWrapperId = 'map';
   const maxZoom = 3;
+  const mapLayers = new Array();
   let currentZoom = 0;
   let propellerAeroMap;
+
+  showItemOnMap = item => {
+    let itemLayer = null;
+    switch (item) {
+      case 'drone':
+        itemLayer = new L.polygon([[75, -140], [75, 80], [0, 80], [0, -140]], {
+          interactive: false,
+          color: 'red',
+          fillColor: '#f03',
+          fillOpacity: 0.3
+        });
+        break;
+
+      case 'flower':
+        itemLayer = new L.Circle([82.5, 163], {
+          interactive: false,
+          radius: 200000
+        });
+        break;
+
+      case 'reset':
+        resetView();
+        break;
+
+      default:
+        break;
+    }
+
+    if (itemLayer) {
+      currentZoom = 1;
+      propellerAeroMap.setView(new L.LatLng(0, 0), currentZoom);
+      mapLayers.push(itemLayer);
+      itemLayer.addTo(propellerAeroMap);
+    }
+  };
+
+  resetView = () => {
+    document.querySelectorAll('#actions .action').forEach(actionBtn => {
+      actionBtn.disabled = false;
+    });
+    mapLayers.forEach(layer => {
+      layer.removeFrom(propellerAeroMap);
+    });
+    currentZoom = 1;
+    propellerAeroMap.setView(new L.LatLng(0, 0), currentZoom);
+  };
+
+  watchForActionClick = () => {
+    document.querySelectorAll('#actions .action').forEach(actionBtn => {
+      actionBtn.addEventListener('click', () => {
+        if (!actionBtn.disabled) {
+          if (actionBtn.dataset.forItem !== 'reset') {
+            actionBtn.disabled = true;
+          }
+          showItemOnMap(actionBtn.dataset.forItem);
+        }
+      });
+    });
+  };
+
   initMap = () => {
-    propellerAeroMap = L.map('map', { maxZoom: maxZoom }).setView(
+    propellerAeroMap = L.map(mapWrapperId, { maxZoom: maxZoom }).setView(
       [0, 0],
       currentZoom
     );
     L.tileLayer('./assets/tiled/{z}/{x}/{y}.jpg', {}).addTo(propellerAeroMap);
+    watchForActionClick();
   };
-  initMap();
+
+  if (document.querySelector(`#${mapWrapperId}`)) {
+    initMap();
+  }
 })();
